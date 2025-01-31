0c81199145c693e4ec2a00613b94ddf024801f8c
Christopher Rieffel <christopher.rieffel@uha.fr>
2025-01-31 10:35:39 +0100

    v.1.1.0

--- src/Fizzbuzz.java
+++ src/Fizzbuzz.java
@@ -1,18 +1,90 @@
+import java.util.HashMap;
+
 public class Fizzbuzz {
 
-    public int number;
+    // -- VARIABLES -- \\
 
-    Fizzbuzz(int number) {
+    public int number;
+    public int[] numbers;
+    public HashMap<Integer, String> config = new HashMap<Integer, String>();
+
+    // -- CONSTRUCTOR -- \\
+
+    public Fizzbuzz() {};
+
+    public Fizzbuzz(int number) {
         this.number = number;
     }
 
-    public void version1() {
-        for (int i = 1; i <= this.number; i++) {
-            if (i % 3 == 0 && i % 5 == 0) System.out.println("FizzBuzz");
-            else if (i % 3 == 0) System.out.println("Fizz");
-            else if (i % 5 == 0) System.out.println("Buzz");
-            else System.out.println(i);
+    // -- HELPERS -- \\
+
+    public void checkNumber(int number) {
+        if (number % 3 == 0 && number % 5 == 0) System.out.println("FizzBuzz");
+        else if (number % 3 == 0) System.out.println("Fizz");
+        else if (number % 5 == 0) System.out.println("Buzz");
+        else System.out.println(number);
+    }
+
+    public String checkNumber2(int number) {
+        String value = "FizzBuzz";
+        if (number % 3 == 0) value = "Fizz";
+        if (number % 5 == 0) value = "Buzz";
+        else value = String.valueOf(number);
+        return value;
+    }
+
+    public StringBuilder checkNumber3(int number) {
+        StringBuilder value = new StringBuilder();
+        if (number % 3 == 0) value.append("Fizz");
+        if (number % 5 == 0) value.append("Buzz");
+        else value.append(number);
+        return value;
+    }
+
+    public void generateNumbers(int number) {
+        for (int i = 0; i <= number; i++) {
+            this.numbers[i] = i;
         }
     }
-    public void version2() {}
+
+    public void generateConfig(int number, String str) {
+        //for (Integer i : config.keySet()) {
+            //if (config.get(i).isEmpty()) config.put(number, str);
+        //}
+        config.put(number, str);
+    }
+
+    // -- METHODS -- \\
+
+    public void version1_0(int number) {
+        for (int i = 1; i <= number; i++) {
+            checkNumber(i);
+        }
+    }
+
+    public void version1_3(int number) {
+        this.generateNumbers(number);
+        for (int num: this.numbers) {
+            checkNumber2(num);
+        }
+    }
+
+    public void version2() {
+        this.generateNumbers(number);
+        for (int num: this.numbers) {
+            checkNumber3(num);
+        }
+    }
+
+    public void version3() {
+        StringBuilder value = new StringBuilder();
+        this.generateNumbers(number);
+        for (int num: this.numbers) {
+            for (Integer i : config.keySet()) {
+                if (num % i == 0) value.append(config.get(i));
+            }
+            if (value.toString().isEmpty()) value.append(num);
+        }
+        System.out.println(value);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
+        Fizzbuzz fb = new Fizzbuzz(20);
+
+        fb.version1_0(10);
+        fb.version1_3(10);
+        fb.version2();
+        fb.version3();
     }
 }
